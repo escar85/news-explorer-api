96d6284b07e5e2061150a87817f7a15eb6119b45
Melnik Dmitry <escar1985@yandex.ru>
2020-11-07 17:14:55 +0300

    проект готов

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const { errors } = require('celebrate');
 const router = require('./routes');
 const mainErrorHandler = require('./middlewares/errors/mainErrorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { validateUser, validateAuth } = require('./middlewares/validation');
-const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
@@ -25,8 +23,6 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(requestLogger);
-router.post('/signin', validateAuth, login);
-router.post('/signup', validateUser, createUser);
 app.use(router);
 app.use(errorLogger);
 
